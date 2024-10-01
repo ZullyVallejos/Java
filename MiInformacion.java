b6323ad2b4aa014560f57c3a3d14095dd1d84e33
Zully <vallejoszully@gmail.com>
2024-10-01 15:24:15 -0400

    Primer commit, agregar proyecto Java

---
+++ MiInformacion.java
@@ -0,0 +1,16 @@
+public class MiInformacion {
+    public static void main(String[] args) {
+        // Definir las variables
+        String nombre = "Zully Vallejos";
+        int edad = 30;
+        String pais = "Paraguay";
+        String ciudad = "Asuncion";
+        String gustos = "bailar y ver peliculas";
+
+        // Imprimir la información utilizando las variables
+        System.out.println("Hola, mi nombre es " + nombre + ".");
+        System.out.println("Tengo " + edad + " años de edad.");
+        System.out.println("Soy de " + pais + " y vivo en la ciudad de " + ciudad + ".");
+        System.out.println("Me gusta mucho " + gustos + ".");
+    }
+}
